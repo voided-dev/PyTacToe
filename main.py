b5e02df9a8f8d6d9c2be1dfe6cf9c87715d14650
voided <voided-dev@proton.me>
2024-11-26 22:13:30 -0400

    Completed X and O Display to the Game

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 import sys
 
 ### Global Scope Variables ###
+pygame.init()    # initalize the pygame library
 WIN_WIDTH = 300
 WIN_HEIGHT = 300
 GRID_SIZE = 100
@@ -28,6 +29,11 @@ def tiktaktoeWinner():
         ((0,0),(1,1),(2,2))  # diagonal from top left to bottom right   
     ]
 
+    # check to see if any of the winning states are all "X" or all "0"
+    for row in winning_combos:
+        pass
+
+
 def displayGrid():
     for x in range(0, WIN_WIDTH, GRID_SIZE): #  start the loop at 0, end at max width of the screen, moving/stepping the length of the grid size
         for y in range(0, WIN_WIDTH, GRID_SIZE):    # see comment above
@@ -35,10 +41,30 @@ def displayGrid():
             pygame.draw.rect(SCREEN, BLACK, grid_rect, 1) # draws/displays the current grid square to the main window
 
 def displaySymbol():
-    for i in range(WIN_WIDTH / GRID_SIZE): # range 3 due to grid being a 3x3
-        for j in range(WIN_HEIGHT / GRID_SIZE):
-            pass
+    #initialize the font
+    symbol_font = pygame.font.SysFont('Times New Roman', 75)
+    
+    #render out the 'X' and 'O'
+    valX = symbol_font.render('X', True, BLACK)
+    valO = symbol_font.render('O', True, BLACK)
 
+    # create a rect around the render text
+    valX_rect = valX.get_rect()
+    valO_rect = valO.get_rect()
+
+    for i in range(int(WIN_WIDTH / GRID_SIZE)):
+        for j in range(int(WIN_HEIGHT / GRID_SIZE)):
+
+            pos_x = i * GRID_SIZE # get the posistion to blit the text index * 100 (grid_size)
+            pos_y = j * GRID_SIZE # get the posistion to blit the text index * 100 (grid_size)
+
+            if grid_state[i][j] == 'X':
+                # center the text rect
+                valX_rect.center = (pos_x + GRID_SIZE // 2, pos_y + GRID_SIZE // 2)
+                SCREEN.blit(valX, valX_rect)
+            if grid_state[i][j] == 'O':
+                valO_rect.center = (pos_x + GRID_SIZE // 2, pos_y + GRID_SIZE // 2)
+                SCREEN.blit(valO, valO_rect)
 def main():
     ### poll display settings ###
     global SCREEN, CLOCK  # declared globally for use in creating functions
@@ -47,7 +73,7 @@ def main():
     CLOCK = pygame.time.Clock()
     running = True  # created variable to switch state to determine if the game is running or not
     player_state = True # created variable to decipher if it is player1 'X' or player2 'O' playing
-    pygame.init()    # initalize the pygame library
+    
 
     while running:
         ### Event Handler ###
@@ -69,9 +95,11 @@ def main():
                     elif player_state == False:   # checks if box has been played and player_state is false(player2), returns 'O'
                         grid_state[grid_x][grid_y] = 'O'
 
-
-                print((grid_x, grid_y), grid_state[grid_x][grid_y], player_state)
-                player_state = not player_state #once one of the above conditions is met change the state of player_state so that it becomes the other players turn
+                    print((grid_x, grid_y), grid_state[grid_x][grid_y], player_state)
+                    player_state = not player_state
+                else:
+                    print(tiktaktoeWinner())
+ #once one of the above conditions is met change the state of player_state so that it becomes the other players turn
 
         ### Rendered Game Code ###
         #   set a colour so the window displays
@@ -79,7 +107,7 @@ def main():
         displayGrid()
         displaySymbol()
         pygame.display.update()
-
+        
         CLOCK.tick(60) #limit 60 FPS, deltatime (dt) not needed for this program since we need no frame-independant physics
 
 
